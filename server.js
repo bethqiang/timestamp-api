60fd14935058d86b607aa0315230d00879b81627
Beth Qiang <bethany.qiang@gmail.com>
2016-10-27 14:49:02 -0500

    Set up server on port

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
+});
