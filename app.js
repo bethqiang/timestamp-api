396d8e07bf9c14cfeca4504c96e2657834d165e3
Beth Qiang <bethany.qiang@gmail.com>
2016-12-13 00:00:30 -0600

    Make app.js work w heroku & delete body-parser
    Not sure how body-parser ended up in here in the first place, heh.

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', require('./routes'));
 
+const port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log(`Contact from intelligent life received on port ${port}`);
 });
