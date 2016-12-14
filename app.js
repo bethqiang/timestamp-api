dcbcb4c0873cdc29793fba8a82a05b5c4b23e04f
Beth Qiang <bethany.qiang@gmail.com>
2016-12-13 19:01:19 -0600

    Add error handling middleware bc that's important

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ const port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log(`Contact from intelligent life received on port ${port}`);
 });
+
+app.use('/', (err, req, res, next) => {
+  console.log('Houston, we have a problem.');
+  res.sendStatus(err.status || 500);
+});
